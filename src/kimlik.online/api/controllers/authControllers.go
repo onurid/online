3a3318ff6f82f8e8af6b5c4c38426e126a078a9d
Onur Yasar <oyasarid@gmail.com>
2020-04-19 18:35:30 +0300

    kimlik.online api development

--- src/kimlik.online/api/controllers/authControllers.go
+++ src/kimlik.online/api/controllers/authControllers.go
@@ -34,6 +34,7 @@ var Authenticate = func(w http.ResponseWriter, r *http.Request) {
 }
 
 var UpdateAccount = func(w http.ResponseWriter, r *http.Request) {
+
 	account := &models.Account{}
 	err := json.NewDecoder(r.Body).Decode(account) //decode the request body into struct and failed if any error occur
 	if err != nil {
@@ -43,4 +44,10 @@ var UpdateAccount = func(w http.ResponseWriter, r *http.Request) {
 	id := r.Context().Value("user").(uint)
 	resp := account.Update(id) //Update account
 	u.Respond(w, resp)
+}
+
+var ValidToken = func(w http.ResponseWriter, r *http.Request) {
+
+	u.Respond(w, u.Message(true, "Token is valid"))
+	return
 }
--- src/kimlik.online/api/main.go
+++ src/kimlik.online/api/main.go
@@ -17,6 +17,7 @@ func main() {
 	router.HandleFunc("/api/user/new", controllers.CreateAccount).Methods("POST")
 	router.HandleFunc("/api/user/login", controllers.Authenticate).Methods("POST")
 	router.HandleFunc("/api/user/update", controllers.UpdateAccount).Methods("POST")
+	router.HandleFunc("/api/token/valid", controllers.ValidToken).Methods("GET")
 	router.HandleFunc("/api/contacts/new", controllers.CreateContact).Methods("POST")
 	router.HandleFunc("/api/me/contacts", controllers.GetContactsFor).Methods("GET") //  user/2/contacts
 
